4cb49d26362412578a10fc2019fc704879fcb099
边宇琨 <bianyukun1213@outlook.com>
2023-02-25 01:11:34 +0800

    现在会显示教学作息时间表

--- timer.js
+++ timer.js
@@ -7,103 +7,103 @@ async function scheduleTimer({ providerRes, parserRes } = {}) {
     const sections = {
         versionA: [{
             section: 1,
-            startTime: '08:20',
-            endTime: '09:00',
+            startTime: '08:30',
+            endTime: '09:15',
         },
         {
             section: 2,
-            startTime: '09:05',
-            endTime: '09:45',
+            startTime: '09:20',
+            endTime: '10:05',
         },
         {
             section: 3,
-            startTime: '10:40',
+            startTime: '10:35',
             endTime: '11:20',
         },
         {
             section: 4,
             startTime: '11:25',
-            endTime: '12:05',
+            endTime: '12:10',
         },
         {
             section: 5,
             startTime: '13:30',
-            endTime: '14:10',
+            endTime: '14:15',
         },
         {
             section: 6,
-            startTime: '14:15',
-            endTime: '14:55',
-        },
-        {
-            section: 7,
-            startTime: '15:30',
-            endTime: '16:10',
-        },
-        {
-            section: 8,
-            startTime: '16:15',
-            endTime: '16:55',
-        },
-        {
-            section: 9,
-            startTime: '18:30',
-            endTime: '19:10',
-        },
-        {
-            section: 10,
-            startTime: '19:15',
-            endTime: '19:55',
-        }],
-        versionB: [{
-            section: 1,
-            startTime: '08:30',
-            endTime: '09:10',
-        },
-        {
-            section: 2,
-            startTime: '09:15',
-            endTime: '09:55',
-        },
-        {
-            section: 3,
-            startTime: '10:25',
-            endTime: '11:05',
-        },
-        {
-            section: 4,
-            startTime: '11:10',
-            endTime: '11:50',
-        },
-        {
-            section: 5,
-            startTime: '13:40',
-            endTime: '14:20',
-        },
-        {
-            section: 6,
-            startTime: '14:25',
+            startTime: '14:20',
             endTime: '15:05',
         },
         {
             section: 7,
-            startTime: '15:20',
-            endTime: '16:00',
+            startTime: '15:30',
+            endTime: '16:15',
         },
         {
             section: 8,
-            startTime: '16:05',
-            endTime: '16:45',
+            startTime: '16:20',
+            endTime: '17:05',
         },
         {
             section: 9,
             startTime: '18:30',
-            endTime: '19:10',
+            endTime: '19:15',
         },
         {
             section: 10,
-            startTime: '19:15',
-            endTime: '19:55',
+            startTime: '19:20',
+            endTime: '20:05',
+        }],
+        versionB: [{
+            section: 1,
+            startTime: '08:30',
+            endTime: '09:15',
+        },
+        {
+            section: 2,
+            startTime: '09:20',
+            endTime: '10:05',
+        },
+        {
+            section: 3,
+            startTime: '10:25',
+            endTime: '11:10',
+        },
+        {
+            section: 4,
+            startTime: '11:15',
+            endTime: '12:00',
+        },
+        {
+            section: 5,
+            startTime: '13:30',
+            endTime: '14:15',
+        },
+        {
+            section: 6,
+            startTime: '14:20',
+            endTime: '15:05',
+        },
+        {
+            section: 7,
+            startTime: '15:30',
+            endTime: '16:15',
+        },
+        {
+            section: 8,
+            startTime: '16:20',
+            endTime: '17:05',
+        },
+        {
+            section: 9,
+            startTime: '18:30',
+            endTime: '19:15',
+        },
+        {
+            section: 10,
+            startTime: '19:20',
+            endTime: '20:05',
         }],
         versionC: [{
             section: 1,
@@ -156,9 +156,17 @@ async function scheduleTimer({ providerRes, parserRes } = {}) {
             endTime: '19:55',
         }]
     };
+    let table = '节次\t教学主楼\t科技大厦\n';
+    for (let index = 0; index < 10; index++) {
+        const aStartTime = sections.versionA[index].startTime;
+        const aEndTime = sections.versionA[index].endTime;
+        const bStartTime = sections.versionB[index].startTime;
+        const bEndTime = sections.versionB[index].endTime;
+        table += `${index + 1}\t${aStartTime}-${aEndTime}\t${bStartTime}-${bEndTime}\n`;
+    }
     const userSelect = await AIScheduleSelect({
         titleText: '选择教学作息时间',
-        contentText: '学校的教学作息时间比较灵活，受接口限制无法具体到每节课设置，只能统一设置，选择最接近的即可。',
+        contentText: `学校的教学作息时间比较灵活，受接口限制无法具体到每节课设置，只能统一设置，选择最接近的即可。\n\n${table}`,
         selectList: [
             '教学主楼',
             '科技大厦' //,
